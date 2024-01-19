020b845764d13ae91eb6092e6352145f9c90a18d
Anna Fedyna <annafedyna03@gmail.com>
2024-01-19 11:32:32 +0000

    27.better practice

--- Arrays/27.remove-element.py
+++ Arrays/27.remove-element.py
@@ -1,15 +1,20 @@
 class Solution:
+    '''
+    Removes all occurrences of a specified value in-place from the given list
+    Parameters:
+        - nums (List[int]): The input list of integers.
+        - val (int): The value to be removed from the list.
+    Returns:
+    - int: The new length of the list after removing all occurrences of the specified value.
+    
+    '''
     def removeElement(self, nums: list[int], val: int) -> int:
-        
+        index = 0
         for i in range(len(nums)):
-            if nums[i] == val:
-                for j in range(i, len(nums)):
-                    if nums[j] != val:
-                       nums[i], nums[j] = nums[j],nums[i]
-                
-        k = len(nums) - len([i for i in nums if i == val])         
-        nums = nums[:k]              
-        return k, nums
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1             
+        return index 
     
 s = Solution()
 print(s.removeElement([1,2,2,3,0,4],2))
