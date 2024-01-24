a3ecb7f5cd1c52c25a9f3e25545e800361cd370f
Anna Fedyna <annafedyna03@gmail.com>
2024-01-24 10:56:34 +0000

    1047.LeetCode

---
+++ Stack/1047.remove-duplicates-string.py
@@ -0,0 +1,13 @@
+def removeDuplicates(s: str) -> str:
+    stack = []
+    for i in s:
+        if stack:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+        else:
+            stack.append(i)
+    return ''.join(stack)
+
+print(removeDuplicates('azxxzy'))
