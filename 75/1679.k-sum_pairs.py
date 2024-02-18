d1828c90e635dc22ee97bd0f0f2b80472860df3a
Anna Fedyna <annafedyna03@gmail.com>
2024-02-18 23:11:59 +0000

    1679.LeetCode

--- 75/1679.k-sum_pairs.py
+++ 75/1679.k-sum_pairs.py
@@ -1,19 +1,8 @@
-def maxOperations(nums: list[int], k: int) -> int:
-    count = 0
-    p1 = 0 
-    p2 = 1
-    while p1 < p2 and p2<len(nums):
-        if nums[p1] + nums[p2] == k:
-            count += 1
-            del nums[p1] 
-            del nums[p2-1]
-            p1 = 0 
-            p2 = 1
-        else:
-            p2 += 1 
-            if p2 == len(nums):
-                p1 += 1
-                p2 = p1 + 1
-    return count
+from collections import Counter
 
-print(maxOperations([1,0,4,2],15))
+# O(n)
+def maxOperations(nums: list[int], k: int) -> int:
+    count = Counter(nums)
+    return sum(min(count[num], count[k-num]) for num in nums) //2
+
+print(maxOperations([1,0,4,2],1))
