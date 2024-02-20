3ebb91579a2162fe9bf5049b2e986c8a826e24a9
Anna Fedyna <annafedyna03@gmail.com>
2024-02-20 23:03:10 +0000

    1456. LeetCode

---
+++ 75/1456.max_vowels.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def maxVowels(s: str, k: int) -> int:
+    v = ['a','e', 'o', 'i', 'u']
+    
+    sub = deque(s[:k])
+  
+    max_vowel = start_vowel = len([i for i in sub if i in v])
+    
+    p = k
+    while p < len(s):
+        
+        letter = sub.popleft()
+        if letter in v:
+            start_vowel -= 1
+            
+        sub.append(s[p])
+  
+        if s[p] in v:
+            start_vowel += 1
+        
+        if start_vowel > max_vowel:
+            max_vowel = start_vowel
+     
+        p+=1
+    return max_vowel
+            
+print(maxVowels('abciiidef', 3))
