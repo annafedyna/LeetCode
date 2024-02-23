7f9844880096350de7b10949a27a83f512b2ed31
Anna Fedyna <annafedyna03@gmail.com>
2024-02-23 22:59:09 +0000

    735. one more function

--- 75/735.asteroid_collision.py
+++ 75/735.asteroid_collision.py
@@ -16,6 +16,22 @@ def asteroidCollision(asteroids: list[int]) -> list[int]:
                 pass    
     return stack
 
+def asteroidCollision2(asteroids: list[int]) -> list[int]:
+    stack = []
+
+    for aster in asteroids:
+        
+        while stack and aster < 0 < stack[-1]:
+                if stack[-1] < -aster:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -aster:
+                    stack.pop()
+                    break
+        else: 
+            stack.append(aster)
+          
+    return stack
 
 print(asteroidCollision([1, 2 ,8,-8, 5, 10,-5]))
 
