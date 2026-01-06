94e17d49cd3393a3b1c7a7d7dc853f5a4a4b3447
anna-fedyna <anna@judgeservice.com>
2026-01-06 17:06:48 +0000

    working solution, fail on time

---
+++ BitManipulation/29.divide_two_integers.py
@@ -0,0 +1,28 @@
+def divide(dividend: int, divisor: int) -> int:
+    if dividend == -2147483648 and divisor == -1 :
+        return 2147483647
+    nagative = False if (dividend >= 0 and divisor >= 0) or (dividend <= 0 and divisor <= 0) else True
+    res = 0
+    i = 0
+
+    if dividend < 0 :
+        dividend = (~dividend) + 1
+    if divisor < 0:
+        divisor = (~divisor) + 1
+
+    while (divisor << i +1 ) < dividend:
+        i += 1
+
+    if i > 0:
+        res += (1 << i)
+        dividend -=  divisor << i
+
+
+    while dividend >= divisor:
+        res += 1
+        dividend -= divisor
+    
+    return (~res) + 1 if nagative else res
+
+print(divide(0, -1))
+print(divide(2147483647, 1))
