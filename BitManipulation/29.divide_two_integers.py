3d383f8217d43cd04fee6ca6dc4f216ff4dde3b5
anna-fedyna <anna@judgeservice.com>
2026-01-07 10:21:18 +0000

    divide two integers passed time limit

--- BitManipulation/29.divide_two_integers.py
+++ BitManipulation/29.divide_two_integers.py
@@ -3,21 +3,22 @@ def divide(dividend: int, divisor: int) -> int:
         return 2147483647
     nagative = False if (dividend >= 0 and divisor >= 0) or (dividend <= 0 and divisor <= 0) else True
     res = 0
-    i = 0
 
     if dividend < 0 :
         dividend = (~dividend) + 1
     if divisor < 0:
         divisor = (~divisor) + 1
 
-    while (divisor << i +1 ) < dividend:
-        i += 1
-
-    if i > 0:
-        res += (1 << i)
-        dividend -=  divisor << i
-
-
+    i = 0  
+    while dividend >= divisor and (divisor << i +1 ) < dividend:
+        while (divisor << i +1 ) < dividend:
+            i += 1
+            
+        if i > 0:
+            res += (1 << i)
+            dividend -=  divisor << i
+        i = 0
+    
     while dividend >= divisor:
         res += 1
         dividend -= divisor
@@ -25,4 +26,5 @@ def divide(dividend: int, divisor: int) -> int:
     return (~res) + 1 if nagative else res
 
 print(divide(0, -1))
+print(divide(16, 3))
 print(divide(2147483647, 1))
